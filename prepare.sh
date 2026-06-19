a4cdc1fe19862009b3d257a51020c2b15ad01deb
Yifeng "Evan" Wang <7312949+doodlewind@users.noreply.github.com>
2026-06-19 12:25:42 +0800

    Install LLVM for PSP SDK builds

--- prepare.sh
+++ prepare.sh
@@ -14,7 +14,8 @@ if [ "$(uname -s)" = "Darwin" ]; then
     autoconf automake cmake \
     libusb libarchive gpgme \
     bash openssl libtool \
-    zlib libmpc makepkg meson
+    zlib libmpc makepkg meson \
+    llvm
     brew reinstall openssl # https://github.com/Homebrew/homebrew-core/issues/169728#issuecomment-2074958306
   fi
 else
@@ -30,25 +31,25 @@ else
 
     ubuntu | linuxmint | debian | pop)
         apt-get update
-        apt-get -y install texinfo bison flex gettext libgmp3-dev libmpfr-dev libmpc-dev libusb-1.0-0-dev libreadline-dev libcurl4 \
+        apt-get -y install clang llvm texinfo bison flex gettext libgmp3-dev libmpfr-dev libmpc-dev libusb-1.0-0-dev libreadline-dev libcurl4 \
         libcurl4-openssl-dev libssl-dev libarchive-dev libgpgme-dev cmake libncurses-dev automake pkg-config \
         wget libtool libz-dev makepkg meson ninja-build
     ;;
     rhel | fedora)
-         dnf -y install @development-tools gcc gcc-c++ g++ wget git autoconf automake make cmake pkgconf \
+         dnf -y install @development-tools clang llvm gcc gcc-c++ g++ wget git autoconf automake make cmake pkgconf \
           libarchive-devel openssl-devel gpgme-devel libtool gettext texinfo bison flex gmp-devel mpfr-devel libmpc-devel ncurses-devel diffutils \
           libusb1-devel readline-devel libcurl-devel which glibc-gconv-extra xz gawk file makepkg meson ninja-build
     ;;
     gentoo)
         emerge --noreplace net-misc/wget dev-vcs/git sys-apps/fakeroot \
                                         app-arch/libarchive app-crypt/gpgme sys-devel/bison sys-devel/flex\
-                                        dev-libs/mpc dev-libs/libusb dev-build/ninja dev-build/meson
+                                        sys-devel/clang sys-devel/llvm dev-libs/mpc dev-libs/libusb dev-build/ninja dev-build/meson
     ;;
     arch | manjaro | endeavouros | cachyos)
-        pacman -Sy gcc clang make cmake patch git texinfo flex bison gettext wget gsl gmp mpfr libmpc libusb readline libarchive gpgme bash openssl libtool boost meson ninja
+        pacman -Sy gcc clang llvm make cmake patch git texinfo flex bison gettext wget gsl gmp mpfr libmpc libusb readline libarchive gpgme bash openssl libtool boost meson ninja
     ;;
     opensuse*)
-      zypper install -y gcc gcc-c++ clang binutils patch make cmake bison flex gpgme libgpgme-devel libarchive-devel openssl libopenssl-devel ncurses ncurses-devel gmp-devel mpfr-devel mpc-devel \
+      zypper install -y gcc gcc-c++ clang llvm binutils patch make cmake bison flex gpgme libgpgme-devel libarchive-devel openssl libopenssl-devel ncurses ncurses-devel gmp-devel mpfr-devel mpc-devel \
       automake meson ninja
     ;;
     *)
